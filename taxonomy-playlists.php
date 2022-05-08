47d57fb5de70390c03b5b08c2e85339303da7092
edris <edris.qeshm2@gmail.com>
2022-05-08 15:44:37 +0430

    share links became dynamic

--- taxonomy-playlists.php
+++ taxonomy-playlists.php
@@ -59,21 +59,21 @@ switch ($playlist_type) {
       </div>
     </button>
     <div id="shareMenu">
-        <button class="button-container button-56" onclick="copyUrl()">
-            <div class="button-face ghost-button">
-                <img class="button-icon" src="<?php echo get_template_directory_uri(); ?>/icons/copy.svg" alt="">
-            </div>
-        </button>
-        <a class="button-container button-56" href="https://wa.me/?text={url}%0A{pageTitle}" target="_blank">
-            <div class="button-face ghost-button">
-                <img class="button-icon" src="<?php echo get_template_directory_uri(); ?>/icons/whatsappt.svg" alt="">
-            </div>
-        </a>
-        <a class="button-container button-56" href="https://t.me/share/url?url={url}&text={pageTitle}" target="_blank">
-            <div class="button-face ghost-button">
-                <img class="button-icon" src="<?php echo get_template_directory_uri(); ?>/icons/telegram.svg" alt="">
-            </div>
-        </a>
+      <button class="button-container button-56" onclick="copyUrl()">
+        <div class="button-face ghost-button">
+          <img class="button-icon" src="<?php echo get_template_directory_uri(); ?>/icons/copy.svg" alt="">
+        </div>
+      </button>
+      <a class="button-container button-56" href="https://wa.me/?text=<?php echo get_the_permalink(); ?>%0A<?php echo single_term_title('', false); ?>" target="_blank">
+        <div class="button-face ghost-button">
+          <img class="button-icon" src="<?php echo get_template_directory_uri(); ?>/icons/whatsappt.svg" alt="">
+        </div>
+      </a>
+      <a class="button-container button-56" href="https://t.me/share/url?url=<?php echo get_the_permalink(); ?>&text=<?php echo single_term_title('', false); ?>" target="_blank">
+        <div class="button-face ghost-button">
+          <img class="button-icon" src="<?php echo get_template_directory_uri(); ?>/icons/telegram.svg" alt="">
+        </div>
+      </a>
     </div>
   </header>
 

67cea4e01ffb18db83272311e4a796713f1ee5ff
Adib Amini <4clil3@gmail.com>
2022-05-08 13:56:46 +0430

    Moved Share Menu to their own files

--- single-standard.php
+++ single-standard.php
@@ -30,6 +30,23 @@ $found_posts_count = $query->found_posts;
         <div class="button-hover"></div>
       </div>
     </button>
+    <div id="shareMenu">
+        <button class="button-container button-56" onclick="copyUrl()">
+            <div class="button-face ghost-button">
+                <img class="button-icon" src="<?php echo get_template_directory_uri(); ?>/icons/copy.svg" alt="">
+            </div>
+        </button>
+        <a class="button-container button-56" href="https://wa.me/?text={url}%0A{pageTitle}" target="_blank">
+            <div class="button-face ghost-button">
+                <img class="button-icon" src="<?php echo get_template_directory_uri(); ?>/icons/whatsappt.svg" alt="">
+            </div>
+        </a>
+        <a class="button-container button-56" href="https://t.me/share/url?url={url}&text={pageTitle}" target="_blank">
+            <div class="button-face ghost-button">
+                <img class="button-icon" src="<?php echo get_template_directory_uri(); ?>/icons/telegram.svg" alt="">
+            </div>
+        </a>
+    </div>
   </header>
 
   <article class="single single-blog">

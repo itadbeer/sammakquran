4a455904ddc88a51880e2f64339c0fac4c715092
edris <edris.qeshm2@gmail.com>
2022-06-16 15:46:55 +0430

    versioning js files

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@
 <div id="snackbar">
     <div class="snackbar-message"></div>
 </div>
-
-<script src="<?php echo get_template_directory_uri(); ?>/scripts/main.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/scripts/menu-handler.js"></script>
+<?php $version = wp_get_theme()->get('Version'); ?>
+<script src="<?php echo get_template_directory_uri(); ?>/scripts/main.js?v=<?php echo $version; ?>"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/scripts/menu-handler.js?v=<?php echo $version; ?>"></script>
 </body>
 
 </html>

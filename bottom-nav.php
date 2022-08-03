25ea85566063ba29842a8869b2f9d2bcd4227b23
Adib Amini <50153864+helloadib@users.noreply.github.com>
2022-08-03 22:48:28 +0430

    Issue #15 for main

--- bottom-nav.php
+++ bottom-nav.php
@@ -1,5 +1,12 @@
 <nav class="bottom-nav">
-    <ul class="bottom-nav-face revealable">
+    <ul class="bottom-nav-face">
         <?php echo SammakQuran_get_menu_items('footer'); ?>
     </ul>
+    <a class="button-container button-56 nav-fab" href="https://wa.me/989389218009" target="_blank">
+      <div class="button-face green-button">
+        <img class="button-icon" src="<?php echo get_template_directory_uri(); ?>/icons/support.svg" alt="پشتیبانی">
+        <div class="button-glow"></div>
+        <div class="button-hover"></div>
+      </div>
+    </a>
 </nav>

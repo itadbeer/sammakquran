da29b4d414be6901926c5761b2c7eb5ca4a7322e
Adib Amini <50153864+helloadib@users.noreply.github.com>
2022-08-10 10:51:04 +0430

    Changed support icon

--- bottom-nav.php
+++ bottom-nav.php
@@ -4,7 +4,7 @@
     </ul>
     <a class="button-container button-56 nav-fab" href="https://wa.me/989389218009" target="_blank">
       <div class="button-face green-button">
-        <img class="button-icon" src="<?php echo get_template_directory_uri(); ?>/icons/support.svg" alt="پشتیبانی">
+        <img class="button-icon" src="<?php echo get_template_directory_uri(); ?>/icons/whatsappt.svg" alt="پشتیبانی">
         <div class="button-glow"></div>
         <div class="button-hover"></div>
       </div>

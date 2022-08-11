7a81cd05087fda3e11f4b93d59f8f4da561a0310
edris <edris.qeshm2@gmail.com>
2022-08-11 14:50:07 +0430

    Using medium size for thumbnails to improve 300% loading time

--- template-parts/content-video.php
+++ template-parts/content-video.php
@@ -15,7 +15,7 @@ if (count($video_src[2]) > 0) {
             <div class="thumbnail-container">
                 <?php
                 $thumbnail_id = get_post_thumbnail_id(get_the_ID());
-                $thumbnail_src = get_the_post_thumbnail_url() ? get_the_post_thumbnail_url() : get_template_directory_uri() . "/images/placeholder.svg";
+                $thumbnail_src = get_the_post_thumbnail_url() ? get_the_post_thumbnail_url(get_the_ID(), 'medium', NULL) : get_template_directory_uri() . "/images/placeholder.svg";
                 $thumbnail_alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true) == "" ? get_the_title() : get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
                 ?>
                 <img class="thumbnail" src="<?php echo $thumbnail_src; ?>" alt="<?php echo $thumbnail_alt ?>" loading="lazy">
